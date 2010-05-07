d1985521aa1d0b7e5a034684c34cc686b4c974a8
robbo <dcchut@gmail.com>
2010-05-07 10:39:53 +1000

    added checks for niceness

--- mcount.py
+++ mcount.py
@@ -2,6 +2,11 @@ import os
 
 # count the number of music files in a directory
 def mcount(dir):
+
+	# can't walk over it, so we find nothing
+	if (not os.path.isdir(dir)):
+		return 0
+		
 	i = 0
 
 	# check each file in dir
@@ -16,26 +21,19 @@ def mcount(dir):
 def mcount_valid_ext(ext):
 	ext = str(ext).lower()
 	
-	# these are valid music files
+	# these are valid file extensions
 	valid   = ['.mp3', '.wma', '.m4a', '.flac', '.wv']
-	
-	# these extensions are never music files
-	invalid = ['.jpg', '.ini', '.db', '.txt', '.avi', '.nfo', '.txt', '.rtf', '.jpeg', '.ds_store']
-	
-	if ext in valid:
-		return True
-		
-	# assume that anything else is a music file, probably a bit optimistic
-	return (ext not in invalid)
+
+	return (ext in valid)
 	
 # total number of music files
 total = 0
 
 # list of directories in which to count music
-dirs = open('dirs', 'rb').readlines()
+dirlist = open('dirs', 'rb').readlines()
 
 # consider each directory
-for dir in dirs:
+for dir in dirlist:
 	dir = dir.strip()
 	
 	total += mcount(dir)
