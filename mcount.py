462b3f983d264bbf2a917975fa186d0175a747cc
robbo <dcchut@gmail.com>
2010-05-07 11:23:04 +1000

    fixed up to become more cool

--- mcount.py
+++ mcount.py
@@ -1,41 +1,54 @@
-import os
+import optparse, os
 
-# count the number of music files in a directory
-def mcount(dir):
-
-	# can't walk over it, so we find nothing
-	if (not os.path.isdir(dir)):
-		return 0
-		
+def mcount(dir, valid_extensions):
 	i = 0
-
-	# check each file in dir
+	
 	for wd in os.walk(dir):
-		for name in wd[2]:
-			if (mcount_valid_ext(os.path.splitext(name)[1])):
+		for filename in wd[2]:
+			if (os.path.splitext(filename)[1][1:] in valid_extensions):
 				i += 1
-				
+
 	return i
-
-# is ext a valid extension
-def mcount_valid_ext(ext):
-	ext = str(ext).lower()
-	
-	# these are valid file extensions
-	valid   = ['.mp3', '.wma', '.m4a', '.flac', '.wv']
-
-	return (ext in valid)
-	
-# total number of music files
-total = 0
-
-# list of directories in which to count music
-dirlist = open('dirs', 'rb').readlines()
-
-# consider each directory
-for dir in dirlist:
-	dir = dir.strip()
-	
-	total += mcount(dir)
 		
-print 'you have', total, 'music files on your computer'
+def main():
+	p = optparse.OptionParser(description='Counts the number of music files in a directory',
+						      prog = 'mcount',
+							  version = 'mcount 0.1')
+	p.add_option('-f', help='file containing list of music file extensions (comma seperated list)')
+	p.add_option('-e', help='comma seperated list of file extensions', default ='mp3,wma,m4a,flac,wv')
+	p.add_option('-q', action='store_true', default = False, help='quiet mode')
+	
+	options, arguments = p.parse_args()
+	
+	valid_extensions = []
+	
+	# get any extensions added from the cmd line
+	if (options.e is not None):
+		valid_extensions += options.e.split(',')
+	
+	# get any stored in a defined file
+	if (options.f is not None and os.path.exists(options.f)):
+		valid_extensions += map(lambda x: x.strip(), open(options.f, 'rb').readlines())
+	
+	# get rid of dupes
+	valid_extensions = list(set(valid_extensions))
+
+	# what directories to search through
+	if (len(arguments) == 0):
+		dirs = [os.getcwd()]
+	else:
+		# we take all the directories which actually exist
+		dirs = filter(lambda x: os.path.exists(x), arguments)
+		
+	# count the number of music files
+	total = reduce(lambda x, y: x + mcount(y, valid_extensions), dirs, 0)
+	
+	# not quiet mode
+	if (not options.q):
+		print p.version, 'by dcchut'
+		print 'searched', len(dirs), 'folder(s),', total, 'music files found',
+	else:
+		print total
+
+if __name__ == '__main__':
+	main()
